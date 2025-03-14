3c8d8f73f7286a3be2589358aaed3676157abc1b
Mamadou Ndione <mndione@gmail.com>
2025-03-14 09:33:55 +0000

    feat: plotting scatter and lingress best fits

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -4,19 +4,26 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
-
+    df = pd.read_csv('epa-sea-level.csv')
 
     # Create scatter plot
-
+    fig, ax = plt.subplots()
+    plt.scatter('Year', 'CSIRO Adjusted Sea Level', data=df)
 
     # Create first line of best fit
-
+    slope, intercept, _, _, _ = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
+    ax.axline((1880, 1880 * slope + intercept), (2050, 2050 * slope + intercept))
 
     # Create second line of best fit
-
-
+    df.set_index('Year', inplace=True)
+    df = df.loc[2000:, ['CSIRO Adjusted Sea Level']]
+    slope, intercept, _, _, _ = linregress(df.index, df['CSIRO Adjusted Sea Level'])
+    ax.axline((2000, 2000 * slope + intercept), (2050, 2050 * slope + intercept))
+    
     # Add labels and title
-
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches)')
+    plt.title('Rise in Sea Level')
     
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
